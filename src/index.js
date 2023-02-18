4e62286744aa5d0749531853baceab507a35ebef
+ <+>
2023-02-17 20:27:58 -0700

    fetch on demand complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let targetedData = e.target.children[1].value
+
+
+    fetch(`http://localhost:3000/movies/${targetedData}`)
+      .then((res) => res.json())
+      .then((dataGiven) => {
+        const title = document.querySelector("section#movieDetails h4")
+        const summary = document.querySelector("section#movieDetails p")
+        title.innerText = dataGiven.title
+        summary.innerText = dataGiven.summary
+      })
+  })
+
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
